9fb621f7f1c69aea2e2ccb830b47cd70880e5eee
zvip100 <hershypod@gmail.com>
2025-02-10 17:01:05 -0500

    update

--- server/app.js
+++ server/app.js
@@ -11,7 +11,15 @@ const app = express();
 const URL = process.env.URL;
 const PORT = process.env.PORT;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://track-your-points-production.up.railway.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
